9a97e0341c41881db63a2da24180239bdae6130d
Soud <78749228+Soud-Haroon@users.noreply.github.com>
2021-08-30 18:06:57 +0500

    chat

--- lib/Chat_Screen/Components/body.dart
+++ lib/Chat_Screen/Components/body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:task_blackwhite_02/My_Buttons/Filloutline_Button.dart';
+import 'package:task_blackwhite_02/chatData/Chat.dart';
 import 'package:task_blackwhite_02/constColors.dart';
 
 class MainBody extends StatelessWidget {
@@ -8,6 +9,8 @@ class MainBody extends StatelessWidget {
     return Column(
       children: [
         Container(
+          padding: EdgeInsets.fromLTRB(
+              kDefaultPadding, 0, kDefaultPadding, kDefaultPadding),
           color: PrimaryGreen,
           child: Row(
             children: [
@@ -16,7 +19,30 @@ class MainBody extends StatelessWidget {
               FillOutlineButton(press: () {}, text: 'Active', isFilled: false),
             ],
           ),
-        )
+        ),
+        Expanded(
+            child: ListView.builder(
+                itemCount: chatsData.length,
+                itemBuilder: (context, index) => ChatCard())),
+      ],
+    );
+  }
+}
+
+class ChatCard extends StatelessWidget {
+  const ChatCard({
+    Key? key,
+    this.chat,
+  }) : super(key: key);
+  final Chat chat;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        CircleAvatar(
+          backgroundImage: AssetImage(chat.image),
+        ),
       ],
     );
   }
