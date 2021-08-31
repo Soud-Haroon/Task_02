eb88b5fcaa7c37799822f782b68f0f9c48508820
Soud <78749228+Soud-Haroon@users.noreply.github.com>
2021-08-31 13:19:36 +0500

    Audio
    Audio message added in message.dart for audio playable

--- lib/messages/messages.dart
+++ lib/messages/messages.dart
@@ -16,6 +16,8 @@ class Messages extends StatelessWidget {
       switch (message.messageType) {
         case ChatMessageType.text:
           return TextMessages(message: message);
+        case ChatMessageType.audio:
+          return AudioMessage(message: message);
         default:
           return SizedBox();
       }
@@ -33,7 +35,6 @@ class Messages extends StatelessWidget {
               backgroundImage: AssetImage('assets/images/user_2.png'),
             ),
           SizedBox(width: kDefaultPadding / 2),
-          // TextMessages(message: message),
           messageContaint(message),
         ],
       ),
@@ -42,3 +43,59 @@ class Messages extends StatelessWidget {
 }
 
 //============================================//
+
+class AudioMessage extends StatelessWidget {
+  final ChatMessage message;
+  const AudioMessage({Key? key, required this.message}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.55,
+      padding: EdgeInsets.symmetric(
+          horizontal: kDefaultPadding * 0.75, vertical: kDefaultPadding / 2.5),
+      decoration: BoxDecoration(
+        color: kPrimaryGreen.withOpacity(message.isSender! ? 1 : 0.1),
+        borderRadius: BorderRadius.circular(30),
+      ),
+      child: Row(
+        children: [
+          Icon(
+            Icons.play_arrow,
+            color: message.isSender! ? Colors.white : kPrimaryGreen,
+          ),
+          Expanded(
+            child: Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: kDefaultPadding / 2),
+              child: Stack(
+                clipBehavior: Clip.none,
+                alignment: Alignment.center,
+                children: [
+                  Container(
+                    width: double.infinity,
+                    height: 2,
+                    color: kPrimaryGreen.withOpacity(0.4),
+                  ),
+                  Positioned(
+                    left: 0,
+                    child: Container(
+                      height: 8,
+                      width: 8,
+                      decoration: BoxDecoration(
+                        color: kPrimaryGreen,
+                        shape: BoxShape.circle,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Text('0.37',
+              style:
+                  TextStyle(color: kContentColorLightTheme.withOpacity(0.32))),
+        ],
+      ),
+    );
+  }
+}
