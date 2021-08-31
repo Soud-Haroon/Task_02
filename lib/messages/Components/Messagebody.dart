cb10a29e797fa5e1066597801f0390bf2a8d6e81
Soud <78749228+Soud-Haroon@users.noreply.github.com>
2021-08-31 11:35:02 +0500

    input field

--- lib/messages/Components/Messagebody.dart
+++ lib/messages/Components/Messagebody.dart
@@ -1,13 +1,38 @@
 import 'package:flutter/material.dart';
-import 'package:task_blackwhite_02/constColors.dart';
+import 'package:task_blackwhite_02/chatData/Chat.dart';
+import 'package:task_blackwhite_02/chatData/ChatMessage.dart';
+import 'chat_input.dart';
 
 class MessageBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Spacer(),
+        Expanded(
+            child: ListView.builder(
+                itemCount: demeChatMessages.length,
+                itemBuilder: (context, index) => Messages(
+                      message: demeChatMessages[index],
+                    ))),
         ChatInputField(),
+      ],
+    );
+  }
+}
+
+class Messages extends StatelessWidget {
+  const Messages({
+    Key? key,
+    @required this.message,
+  }) : super(key: key);
+  final ChatMessage message;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: message.isSender ? MainAxisAlignment.end : MainAxisAlignment.start,
+      children: [
+        Text('Chat Text'),
       ],
     );
   }
@@ -15,80 +40,3 @@ class MessageBody extends StatelessWidget {
 
 //--------------------------------------------------//
 
-class ChatInputField extends StatelessWidget {
-  const ChatInputField({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.symmetric(
-          horizontal: kDefaultPadding, vertical: kDefaultPadding / 2),
-      decoration: BoxDecoration(
-        color: Theme.of(context).scaffoldBackgroundColor,
-        boxShadow: [
-          BoxShadow(
-            offset: Offset(0, 4),
-            blurRadius: 32,
-            color: kPrimaryGreen.withOpacity(0.18),
-          )
-        ],
-      ),
-      child: SafeArea(
-          child: Row(
-        children: [
-          Icon(Icons.mic, color: kPrimaryGreen),
-          SizedBox(width: kDefaultPadding),
-          Expanded(
-              child: Container(
-            padding:
-                EdgeInsets.symmetric(horizontal: kDefaultPadding * 0.75),
-            decoration: BoxDecoration(
-              color: kPrimaryGreen.withOpacity(0.05),
-              borderRadius: BorderRadius.circular(40),
-            ),
-            child: Row(
-              children: [
-                Icon(
-                  Icons.sentiment_satisfied_alt_outlined,
-                  color: Theme.of(context)
-                      .textTheme
-                      .bodyText1
-                      ?.color
-                      ?.withOpacity(0.64),
-                ),
-                SizedBox(width: kDefaultPadding / 4),
-                Expanded(
-                  child: TextField(
-                    decoration: InputDecoration(
-                      hintText: 'Type Message',
-                      border: InputBorder.none,
-                    ),
-                  ),
-                ),
-                Icon(
-                  Icons.attach_file,
-                  color: Theme.of(context)
-                      .textTheme
-                      .bodyText1
-                      ?.color
-                      ?.withOpacity(0.64),
-                ),
-                SizedBox(width: kDefaultPadding / 4),
-                Icon(
-                  Icons.camera_alt_outlined,
-                  color: Theme.of(context)
-                      .textTheme
-                      .bodyText1
-                      ?.color
-                      ?.withOpacity(0.64),
-                ),
-              ],
-            ),
-          )),
-        ],
-      )),
-    );
-  }
-}
